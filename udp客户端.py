093f1af3355a32e06c085dcd0ec402ce8c27270d
syunc <36838802+syunc@users.noreply.github.com>
2018-03-16 08:00:06 +0800

    Update udp客户端.py

--- udp客户端.py
+++ udp客户端.py
@@ -2,7 +2,7 @@
 from socket import *
 from time import ctime
 
-HOST = '118.89.248.99'
+HOST = '118.89.248.99'#这是一台服务器，若想在本地运行把HOST改成localhost,tcp客户端也是同理
 PORT = 21567
 BUFSIZ = 1024
 ADDR = (HOST,PORT)
